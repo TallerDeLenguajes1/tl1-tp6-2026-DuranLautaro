2b316040fee1d65747a93852f435a0c6f9929186
“DuranLautaro” <lautarotuna11@gmail.com>
2026-05-27 12:08:28 -0300

    Trabajando en el ej3

--- Program.cs
+++ Program.cs
@@ -1,26 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
-/*Problema 1*/
-int i;
-Console.WriteLine("Ingresar un numero entero positivo");
-string x=Console.ReadLine();
-bool resultado=int.TryParse(x, out i);// transformo tipo de dato string a int
-Console.WriteLine("valor de i:"+i);
-int invertido=0;
-int aux=i;
-while (resultado && aux>0)
+double c;
+int opcion;
+do
 {
-    int dig=aux%10;
-    invertido=invertido*10+dig;
-    aux=aux/10;
-}
-if(i>0)
-{
-    Console.WriteLine($"Numero invertido:{invertido}");
-}
+    Console.WriteLine("Seleccione la opcion deseada:\n");
+    Console.Write("1.SUMAR,2.RESTAR,3.MULTIPLICAR,4.DIVIDIR");
+    string entrada=Console.ReadLine();
+    if(!int.TryParse(entrada,out opcion))
+    {
+        Console.WriteLine("Numero invalido. Intente nuevamente\n");
+    }
+    else
+    {
+        if(opcion!=0)
+        {
+            Console.Write("Ingresar un numero entero positivo:\n");
+            string n=Console.ReadLine();
+            double.TryParse(n,out c);
+            double r=0;
+            switch(opcion)
+            {
+                //VALOR ABSOLUTO
+                case 1:
+                Math.Abs(c);
+                break;
+                //Cuadrado
+                case 2:
+                Math.Pow(c,2);
+                break;
+                //LA RAIZ CUADRADA
+                case 3:
+                Math.Sqrt(c);
+                break;
+                //SENO
+                case 4:
+                Math.Sin(c);
+                break;
+                //COSENO
+                case 5:
+                Math.Cos(c);
+                break;
+                //PARTE ENTERA DE UN FLOAT
+                case 6:
+                Math.Truncate(c);
+                break;
+                default:
+                Console.Write("Opcion invalida\n");
+                break;
+            }
+            Console.WriteLine("Resultado:"+r);
+        }
+        else
+        {
+            Console.Write("Saliendo del programa...");
+        }
+        Console.Write("Desea realizar otro calculo?Si/No:");
+        string respuesta=Console.ReadLine();
+      
+        if(respuesta.ToLower()=="no") //tolower convierte string en minusculas
+        {
+            opcion=0;
+        }
+    }
+}while(opcion!=0);
+
