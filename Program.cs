d97a6d4d7b4b1663f7bd2523983a88554f565a24
“DuranLautaro” <lautarotuna11@gmail.com>
2026-05-31 11:13:30 -0300

    Ejercicio 4

--- Program.cs
+++ Program.cs
@@ -1,26 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
-/*Problema 1*/
-int i;
-Console.WriteLine("Ingresar un numero entero positivo");
-string x=Console.ReadLine();
-bool resultado=int.TryParse(x, out i);// transformo tipo de dato string a int
-Console.WriteLine("valor de i:"+i);
-int invertido=0;
-int aux=i;
-while (resultado && aux>0)
+string cadena="Hola me llamo Lautaro,";
+Console.Write($"Longitud de la cadena:{cadena.Length}\n");
+string cadena2=" Y estudio ingenieria informatica";
+string concatenar=cadena+cadena2;
+Console.Write(concatenar);
+string subcadena=cadena.Substring(8,5); //substring(posicion,tomo los caracteres desde esa posicion en adelante)
+Console.Write($"\nSubcadena: {subcadena}\n");
+//Metodo Tostring convierte cualquier tipo de dato a string
+Console.Write("Muestro caracter por caracter con foreach:\n");
+foreach (char c in concatenar)
 {
-    int dig=aux%10;
-    invertido=invertido*10+dig;
-    aux=aux/10;
+    Console.Write(c);
 }
-if(i>0)
-{
-    Console.WriteLine($"Numero invertido:{invertido}");
+Console.Write($"Cadena en minusculas:{subcadena.ToLower()}, Cadena en mayusculas: {subcadena.ToUpper()}");
+string cadena3="Cartuchera Libro Cuaderno";
+string[] cadenasinseparador=cadena3.Split(" ");
+Console.Write("\nCadena sin separadores:\n");
+foreach (string cad in cadenasinseparador) {
+    Console.Write(cad);
 }
+
